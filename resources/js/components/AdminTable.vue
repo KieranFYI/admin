0173aab412f09a3ea0fbb3c04b99f275170487dd
Kieran <kieran@andersons.bz>
2023-01-16 05:04:37 +0000

    Add options to admin table

--- resources/js/components/AdminTable.vue
+++ resources/js/components/AdminTable.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card">
-        <div class="card-header">
+        <div class="card-header" v-if="header && !small">
             <h1 class="card-title">{{ title }}</h1>
             <div class="card-tools d-flex">
                 <slot></slot>
@@ -15,8 +15,14 @@
             </div>
         </div>
         <div class="card-body table-responsive p-0">
-            <table class="table table-striped text-nowrap">
-                <thead>
+            <table :class="{
+                    table: true,
+                    'table-sm': small,
+                    'table-stripped': stripped,
+                    'table-bordered': bordered,
+                    'table-hover': hover
+                }">
+                <thead v-if="thead && !small">
                 <tr>
                     <th v-for="(label, column) in columns">
                         <template v-if="typeof label === 'object' && typeof label.label === 'string'">
@@ -29,8 +35,8 @@
                     <th style="width: 100px" class="text-right">Actions</th>
                 </tr>
                 </thead>
-                <tbody>
-                <tr v-if="data.data === null">
+                <tbody v-if="rows !== undefined">
+                <tr v-if="rows === null">
                     <td :colspan="Object.keys(columns).length + 1">
                         <div class="d-flex justify-content-center">
                             <div class="spinner-border text-success" role="status">
@@ -39,13 +45,12 @@
                         </div>
                     </td>
                 </tr>
-                <tr v-else-if="data.data.length === 0">
+                <tr v-else-if="rows.length === 0">
                     <td :colspan="Object.keys(columns).length + 1" class="text-center">
                         No data available
                     </td>
                 </tr>
-                <tr v-for="row in data.data" v-else>
-
+                <tr v-for="row in rows" v-else>
                     <td v-for="(label, column) in columns">
                         <template
                             v-if="typeof label === 'object' && typeof label.type === 'string' && label.type === 'date'">
@@ -62,7 +67,70 @@
                             </div>
                         </div>
                         <template v-else v-for="action in row.actions" v-if="row.actions">
-                            <button type="button" :class="action.class" @click="action.click(row)" v-if="action.click !== undefined" >
+                            <button type="button" :class="action.class" @click="action.click(row)"
+                                    v-if="action.click !== undefined">
+                                <i :class="action.icon"></i>
+                            </button>
+                            <a :href="action.url" class="text-primary" v-else>
+                                <i :class="action.icon"></i>
+                            </a>
+                        </template>
+                    </td>
+                </tr>
+                </tbody>
+                <tbody v-else>
+                <tr v-if="data.data === null">
+                    <td :colspan="Object.keys(columns).length + 1">
+                        <div class="d-flex justify-content-center">
+                            <div class="spinner-border text-success" role="status">
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr v-else-if="data.data.length === 0">
+                    <td :colspan="Object.keys(columns).length + 1" class="text-center">
+                        No data available
+                    </td>
+                </tr>
+                <tr v-for="row in data.data" v-else>
+
+                    <td v-if="actionLocation == 'start'">
+                        <div class="d-flex justify-content-center" v-if="loading">
+                            <div class="spinner-border spinner-border-sm text-secondary" role="status">
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
+                        <template v-else v-for="action in row.actions" v-if="row.actions">
+                            <button type="button" :class="action.class" @click="action.click(row)"
+                                    v-if="action.click !== undefined">
+                                <i :class="action.icon"></i>
+                            </button>
+                            <a :href="action.url" class="text-primary" v-else>
+                                <i :class="action.icon"></i>
+                            </a>
+                        </template>
+                    </td>
+
+                    <td v-for="(label, column) in columns">
+                        <template
+                            v-if="typeof label === 'object' && typeof label.type === 'string' && label.type === 'date'">
+                            {{ this.formatDate(this.getProp(row, column, 'Unknown')) }}
+                        </template>
+                        <template v-else>
+                            {{ this.getProp(row, column, 'Unknown') }}
+                        </template>
+                    </td>
+
+                    <td class="text-right" v-if="actionLocation == 'end'">
+                        <div class="d-flex justify-content-center" v-if="loading">
+                            <div class="spinner-border spinner-border-sm text-secondary" role="status">
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
+                        <template v-else v-for="action in row.actions" v-if="row.actions">
+                            <button type="button" :class="action.class" @click="action.click(row)"
+                                    v-if="action.click !== undefined">
                                 <i :class="action.icon"></i>
                             </button>
                             <a :href="action.url" class="text-primary" v-else>
@@ -74,24 +142,29 @@
                 </tbody>
             </table>
         </div>
-        <div class="card-footer">
+        <div class="card-footer" v-if="footer && !small">
             <div class="row">
                 <div class="col-sm-12 col-md-5">
                     <div class="spinner-border spinner-border-sm text-secondary" role="status" v-if="loading">
                         <span class="sr-only">Loading...</span>
                     </div>
                     <div class="text-muted" v-else>
-                        <template v-if="data.from === 1 && data.to === data.total">
-                            Showing {{ data.total }} entries
+                        <template v-if="rows !== undefined">
+                            Showing {{ rows.total }} entries
                         </template>
-                        <template v-else>
-                            Showing {{ data.from }} to {{ data.to }} of {{ data.total }} entries
+                        <template v-else-if="data !== undefined">
+                            <template v-if="data.from === 1 && data.to === data.total">
+                                Showing {{ data.length }} entries
+                            </template>
+                            <template v-else>
+                                Showing {{ data.from }} to {{ data.to }} of {{ data.total }} entries
+                            </template>
                         </template>
                     </div>
                 </div>
                 <div class="col-sm-12 col-md-7 d-flex justify-content-end">
                     <ul class="pagination pagination-sm m-0">
-                        <template v-for="link in data.links">
+                        <template v-for="link in data.links" v-if="data !== undefined">
                             <li :class="{'page-item': true, disabled: link.active || link.url === null}"
                                 style="cursor: pointer"
                                 @click="page(link)">
@@ -118,12 +191,13 @@ export default {
     ],
     props: {
         title: {
-            type: String,
-            required: true
+            type: String
         },
         data: {
             type: Object,
-            required: true
+        },
+        rows: {
+            type: Array
         },
         columns: {
             type: Object,
@@ -135,6 +209,38 @@ export default {
         search: {
             type: Boolean,
             default: true
+        },
+        header: {
+            type: Boolean,
+            default: true
+        },
+        thead: {
+            type: Boolean,
+            default: true
+        },
+        footer: {
+            type: Boolean,
+            default: true
+        },
+        small: {
+            type: Boolean,
+            default: false,
+        },
+        stripped: {
+            type: Boolean,
+            default: false
+        },
+        bordered: {
+            type: Boolean,
+            default: false
+        },
+        hover: {
+            type: Boolean,
+            default: true
+        },
+        actionLocation: {
+            type: String,
+            default: 'end'
         }
     },
     data() {
