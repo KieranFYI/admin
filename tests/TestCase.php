3bf014c8bc2e9d079258135335cb48060586c569
Kieran Anderson <kieran@andersons.bz>
2023-02-06 03:56:18 +0000

    Tests

---
+++ tests/TestCase.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace KieranFYI\Tests\Admin;
+
+use Illuminate\Foundation\Application;
+use JeroenNoten\LaravelAdminLte\AdminLteServiceProvider;
+use KieranFYI\Admin\Providers\AdminPackageServiceProvider;
+use KieranFYI\Logging\Providers\LoggingPackageServiceProvider;
+use KieranFYI\Misc\Providers\MiscPackageServiceProvider;
+use KieranFYI\Roles\Core\Providers\RolesCorePackageServiceProvider;
+use KieranFYI\Services\Core\Providers\ServicesCorePackageServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+
+class TestCase extends OrchestraTestCase
+{
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Load package service provider.
+     *
+     * @param Application $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            MiscPackageServiceProvider::class,
+            LoggingPackageServiceProvider::class,
+            ServicesCorePackageServiceProvider::class,
+            RolesCorePackageServiceProvider::class,
+            AdminLteServiceProvider::class,
+            AdminPackageServiceProvider::class,
+        ];
+    }
+}
